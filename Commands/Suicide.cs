e6d8830c22d32bef641d1b995d281805714867c8
Jatc252 <42489571+Jatc252@users.noreply.github.com>
2021-08-11 21:37:13 +1000

    Update Suicide.cs

--- Commands/Suicide.cs
+++ Commands/Suicide.cs
@@ -4,7 +4,6 @@ using System;
 
 namespace AnytimeKill.Commands
 {
-    [CommandHandler(typeof(RemoteAdminCommandHandler))]
     [CommandHandler(typeof(GameConsoleCommandHandler))]
     class Suicide : ICommand
     {
