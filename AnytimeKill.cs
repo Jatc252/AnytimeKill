709eb189dd3643f8aea473fd24df051aeb9416de
Jatc252 <42489571+Jatc252@users.noreply.github.com>
2021-10-01 11:03:59 +1000

    exiled 3

--- AnytimeKill.cs
+++ AnytimeKill.cs
@@ -10,7 +10,7 @@ namespace AnytimeKill
         public override string Name { get; } = "AnytimeKill";
         public override string Author { get; } = "Jatc251";
         public override Version Version { get; } = new Version(1, 1, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 11, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
         public override string Prefix { get; } = "anytimekill";
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
         public static AnytimeKill Instance => Singleton;
